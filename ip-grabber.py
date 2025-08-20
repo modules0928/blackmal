c461b5cd2cf51dc958f9ff33f995bc5146528990
modules0928 <anaguimaraes1414@gmail.com>
2025-08-20 00:28:39 -0300

    Update ip-grabber.py

--- ip-grabber.py
+++ ip-grabber.py
@@ -1,11 +1,1 @@
-import requests
-import json
 
-def ip_grabber():
-    ip = requests.get('https://api.ipify.org?format=json').text
-    parse = json.loads(ip)
-    ip_t = parse['ip']
-    geolocation = requests.get(f'http://ip-api.com/json/{ip_t}').text
-    return geolocation
-
-print(ip_grabber())
